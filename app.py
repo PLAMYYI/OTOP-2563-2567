b55c49bc46269d8c2e2d658f27ca16db21b33d67
KasidetDuangthong <ksididechdwngthxng@gmail.com>
2026-03-11 23:43:57 +0700

    resolve merge conflict

--- app.py
+++ app.py
@@ -6,22 +6,21 @@ app = dash.Dash(__name__, use_pages=True)
 
 server = app.server
 
-app.layout = html.Div([
-
-    html.H1("OTOP Dashboard", style={"textAlign": "center"}),
-
-    # เมนูเปลี่ยนหน้า
-    html.Div([
-        dcc.Link("Overview", href="/", style={"marginRight": "20px"}),
-        dcc.Link("Growth Analysis", href="/analysis"),
-    ], style={"textAlign": "center", "marginBottom": "30px"}),
-
-    html.Hr(),
-
-    # ตำแหน่งที่ page จะถูกโหลด
-    dash.page_container
-
-])
+app.layout = html.Div(
+    [
+        html.H1("OTOP Dashboard", style={"textAlign": "center"}),
+        # เมนูเปลี่ยนหน้า
+        html.Div(
+            [
+                dcc.Link("Overview", href="/", style={"marginRight": "20px"}),
+            ],
+            style={"textAlign": "center", "marginBottom": "30px"},
+        ),
+        html.Hr(),
+        # ตำแหน่งที่ page จะถูกโหลด
+        dash.page_container,
+    ]
+)
 
 if __name__ == "__main__":
     app.run(debug=True)
